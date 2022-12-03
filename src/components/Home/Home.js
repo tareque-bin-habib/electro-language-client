053d6faf48a0fe380d26bf3c858265603db57b3e
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-04 00:40:29 +0600

    link added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 import img from '../../image/banner.png'
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className='container mx-auto row row-cols-lg-2 row-cols-md-2 row-cols-sm-1'>
+        <div className='container mx-auto row row-cols-lg-2 row-cols-md-1 row-cols-sm-1'>
             <div className='d-flex justify-content-center align-items-center'>
                 <div>
                     <p className='fw-bold discover'>Discover your journey</p>
@@ -17,7 +18,7 @@ const Home = () => {
                     <p className='banner-pera'>Take your learning organization to the next level.Who'll share their <br />
                         knowledge to people around the world
                     </p>
-                    <Button variant="primary" className='mt-3 ' size="lg">view all courses</Button>
+                    <Link to='/courses'><Button variant="primary" className='mt-3 ' size="lg">view all courses</Button></Link>
                 </div>
             </div>
             <div >
