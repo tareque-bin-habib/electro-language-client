5064a0c0a1632a5cef3d297b1ab1777a6bf332d2
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-04 00:36:59 +0600

    home banner added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import img from '../../image/banner.png'
+import './Home.css'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='container mx-auto row row-cols-lg-2 row-cols-md-2 row-cols-sm-1'>
+            <div className='d-flex justify-content-center align-items-center'>
+                <div>
+                    <p className='fw-bold discover'>Discover your journey</p>
+                    <h1 className='banner-heading'>Discover <span className='number'>4500+</span> Courses <br />
+                        from top Instructors
+                        <br />
+                        Around the world
+                    </h1>
+                    <p className='banner-pera'>Take your learning organization to the next level.Who'll share their <br />
+                        knowledge to people around the world
+                    </p>
+                    <Button variant="primary" className='mt-3 ' size="lg">view all courses</Button>
+                </div>
+            </div>
+            <div >
+                <img src={img} alt="" className='img-fluid' />
+            </div>
         </div>
     );
 };
